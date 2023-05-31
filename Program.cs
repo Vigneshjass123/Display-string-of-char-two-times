c638ea6472262eca4f514760edae47e1a63b108c
Cyntexia <Cyntexia@DESKTOP-R5AAUJH>
2023-06-01 00:16:16 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "sam";
+
+
+            Console.WriteLine("s =" + s[0] + "" + s[0]);
+            Console.WriteLine("a=" + s[1] + "" + s[1]);
+            Console.WriteLine("m=" + s[2] + "" + s[2]);
+            Console.ReadKey();
+        }
+    }
+}
+    
+
